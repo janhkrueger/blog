5ea0fb0429b623292f683bcf2d0cdf3c6448b9fe
janhkrueger <janhkrueger@outlook.com>
2020-09-17 20:31:00 +0200

    Initial commit

--- pelicanconf.py
+++ pelicanconf.py
@@ -2,14 +2,22 @@
 # -*- coding: utf-8 -*- #
 from __future__ import unicode_literals
 
-AUTHOR = 'thedude'
-SITENAME = 'Example Pelican website using GitLab Pages!'
-SITEURL = ''
+# Generelle Blogeinstellungen
+AUTHOR = u'Jan H. Krueger'
+SITENAME = u'JHK Blog'
+SITEURL = 'https://janhkrueger.de/blog/'
+SITESUBTITLE = 'Das private Blog von Jan H. Krueger'
+TIMEZONE = 'Europe/Berlin'
+
+DEFAULT_CATEGORY = 'Allgemein'
+USE_FOLDER_AS_CATEGORY=True
 
 PATH = 'content'
 OUTPUT_PATH = 'public'
 
-TIMEZONE = 'Europe/Athens'
+# global metadata to all the contents
+DEFAULT_METADATA = (('jhk', 'blog'),)
+
 
 DEFAULT_LANG = 'en'
 
@@ -20,17 +28,27 @@ TRANSLATION_FEED_ATOM = None
 AUTHOR_FEED_ATOM = None
 AUTHOR_FEED_RSS = None
 
-# Blogroll
-LINKS = (('Pelican', 'http://getpelican.com/'),
-         ('Python.org', 'http://python.org/'),
-         ('Jinja2', 'http://jinja.pocoo.org/'),
-         ('You can modify those links in your config file', '#'),)
+# Blogroll, Links im Seitenmenue
+LINKS=(
+    ('Piratenpartei','https://piratenpartei.de'),
+    ('Free Software Foundation Europe','https://fsfe.org'),
+    ('The game without name','http://tgwnn.rpgame.de/'),)
 
 # Social widget
-SOCIAL = (('You can add links in your config file', '#'),
-          ('Another social link', '#'),)
+# Nur ein direkter Zugriff auf die RSS-Feeds
+SOCIAL = (
+    ('Twitter', 'https://twitter.com/janhkrueger'), 
+    ('gitlab', 'https://gitlab.com/janhkrueger'),
+    ('RSS', 'https://janhkrueger.de/blog/feeds/all.rss.xml'),)
+
+# Twitter
+TWITTER_USERNAME='janhkrueger'
+TWITTER_CARDS=True
 
 DEFAULT_PAGINATION = 5
+
+# Lizenz unter welcher das Blog betrieben wird
+CC_LICENSE = 'CC-BY-NC-SA'
 
 # Uncomment following line if you want document-relative URLs when developing
 #RELATIVE_URLS = True
